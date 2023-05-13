9a44380566fb3d0339e4259afc9fd4ede88ac9d1
imran_72 <hossain1707072@stud.kuet.ac.bd>
2023-05-14 00:42:38 +0600

    Password change Api is checked and found all okay

---
+++ src/main/java/com/example/university_management/module/passwordUpdate/PasswordService.java
@@ -0,0 +1,28 @@
+package com.example.university_management.module.passwordUpdate;
+
+import com.example.university_management.acl.auth.entity.User;
+import com.example.university_management.acl.auth.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordService {
+
+    @Autowired
+    private UserRepo userRepo;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Boolean passwordChange(String password) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUserName = authentication.getName();
+        User user = userRepo.findByUserName(currentUserName);
+        user.setPassword(bCryptPasswordEncoder.encode(password));
+        userRepo.save(user);
+        return true;
+    }
+
+}
